58b25fc772031a96db6fd014f5ee083460c3f62a
Fratzinger <22286818+fratzinger@users.noreply.github.com>
2020-03-15 22:59:50 +0100

    test(server): add istanbul and add app.tests
    - 404
    - static uploads

--- packages/server/test/app.test.ts
+++ packages/server/test/app.test.ts
@@ -1,9 +1,17 @@
 import { expect } from "chai";
 import { Server } from "http";
+import axios from "axios";
+import url from "url";
 
 import app from "../src/app";
 
-const port = app.get("port") || 8998;
+const port = app.get("port") || 3030;
+const getUrl = (pathname?: string): string => url.format({
+  hostname: app.get("host") || "localhost",
+  protocol: "http",
+  port,
+  pathname
+});
 
 const servicePaths = [
   "api/v1/activities",
@@ -28,6 +36,48 @@ describe("Feathers application tests", () => {
 
   after(function(done) {
     server.close(done);
+  });
+
+  it("starts and shows the index page", async () => {
+    const { data } = await axios.get(getUrl());
+    expect(data.indexOf("<!DOCTYPE html>") !== -1, "starts with <html> tag").to.be.ok;
+  });
+
+  describe("404", function() {
+    it("shows a 404 HTML page", async () => {
+      try {
+        await axios.get(getUrl("path/to/nowhere"), {
+          headers: {
+            "Accept": "text/html"
+          }
+        });
+        expect.fail("should never get here");
+      } catch (error) {
+        const { response } = error;
+
+        expect(response.status, "is 404").to.equal(404);
+        expect(response.data.indexOf("<html>") !== -1, "is html").to.be.true;
+      }
+    });
+
+    it("shows a 404 JSON error without stack trace", async () => {
+      try {
+        await axios.get(getUrl("path/to/nowhere"));
+        expect.fail("should never get here");
+      } catch (error) {
+        const { response } = error;
+
+        expect(response.status, "status is 404").to.equal(404);
+        expect(response.data.code, "code is 404").to.equal(404);
+        expect(response.data.message, "Page not found").to.equal("Page not found");
+        expect(response.data.name, "NotFound").to.equal("NotFound");
+      }
+    });
+  });
+
+  it("has uploads static and profilePic loading", async () => {
+    const response = await axios.get(getUrl("uploads/profilePic.jpg"));
+    expect(response.status, "loaded profilePic").to.equal(200);
   });
 
   it("registeres all services", () => {
